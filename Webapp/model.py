0c754f852f9fa613d4a49cc55a71bb0b0373ca12
marsh52 <marshallprose9@gmail.com>
2025-01-15 00:50:49 -0500

    webapp update

--- Webapp/model.py
+++ Webapp/model.py
@@ -3,15 +3,15 @@ import numpy as np
 
 # Load the saved Random Forest model
 model = joblib.load('random_forest_model.pkl')
-scaler = joblib.load('scaler.pkl')
+#scaler = joblib.load('scaler.pkl')
 
 def predict_sleep(workout_time, reading_time, phone_time, work_hours, caffeine_intake, relaxation_time):
     # Prepare the input as a 2D array
     input_data = np.array([[workout_time, reading_time, phone_time, work_hours, caffeine_intake, relaxation_time]])
     
     # Scale Data
-    scaled = scaler.transform(input_data)
+    #scaled = scaler.transform(input_data)
 
     # Make a prediction
-    prediction = model.predict(scaled)
+    prediction = model.predict(input_data)
     return round(prediction[0], 2)  # Return the predicted sleep hours, rounded to 2 decimal places
